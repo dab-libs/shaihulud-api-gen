c88cc0841c8a2eb569f4a4f2b7b2bd198078dbb9
Dmitry Bystrov <bystrov.yoso@gmail.com>
2021-08-18 11:07:33 +0300

    Отменяем комиты "Исправляем сборку" и "Собираем артефакт в виде одного jar-файла"

--- build.sh
+++ build.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-version="$1"
-
-rm -f "yoso-apigen.jar"
-rm -f "version"
-
-cp -f "src/shaihulud.jar" "yoso-apigen.jar"
-echo "$version" > "version"
-cd "src"
-jar uf "../yoso-apigen.jar" "php7-client-guzzle" "php8-server-symfony" "../version"
-cd ../
+rm -rdf "yoso-apigen"
+cp -r "src" "yoso-apigen"
