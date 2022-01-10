ab30ac525939c7042847ac532b5a0742de70cf9c
Dmitry Bystrov <bystrov.yoso@gmail.com>
2022-01-10 14:07:34 +0300

    Собираем артэфакты в виде одного jar-файла

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-rm -rdf "yoso-apigen"
-cp -r "src" "yoso-apigen"
+version="$1"
+
+cp ./src/shaihulud.jar yoso-apigen.jar
+echo "$version" > "version"
+jar uf yoso-apigen.jar version
+jar uf yoso-apigen.jar -C src schema.json
+jar uf yoso-apigen.jar -C src php7-client-guzzle
+jar uf yoso-apigen.jar -C src php8-server-symfony
+rm version
